5d371df614362a2f21cf96f3ee02e91000437ed0
Son Daehyeon <sondaehyeon01@gmail.com>
2025-04-13 17:37:17 +0900

    rollback(header): 555e3ff9

--- src/component/layout/header/index.tsx
+++ src/component/layout/header/index.tsx
@@ -58,8 +58,8 @@ export default function Header() {
   if (!user) return null;
 
   return (
-    <header className="fixed top-0 right-0 left-0 z-50 flex h-14 items-center justify-center border-b bg-white">
-      <div className="flex w-2xl items-center justify-between px-4">
+    <header className="fixed top-0 right-0 left-0 z-50 flex h-14 items-center justify-between border-b bg-white px-4">
+      <div className="flex gap-8">
         <Link href="/">
           <Image
             src={TablyLogo}
@@ -84,46 +84,43 @@ export default function Header() {
             </Link>
           ))}
         </nav>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="text-xs sm:text-sm">
-              <User className="size-3.5 sm:size-4" />
-              <div className="flex gap-1">
-                {user.name}
-                <span className="text-xs text-neutral-500 sm:text-sm">({clubName(user.club)})</span>
-              </div>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>동아리</DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                  {Object.keys(Club).map((club) => (
-                    <DropdownMenuItem
-                      key={club}
-                      className="justify-between"
-                      onClick={() => changeClub(club as Club)}
-                    >
-                      {clubName(club)}
-                      {club === user.club && <Check />}
-                    </DropdownMenuItem>
-                  ))}
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="flex text-red-500"
-              onClick={() => Api.Request.removeToken()}
-            >
-              <LogOut color={colors.red['500']} />
-              로그아웃
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="text-xs sm:text-sm">
+            <User className="size-3.5 sm:size-4" />
+            <div className="flex gap-1">
+              {user.name}
+              <span className="text-xs text-neutral-500 sm:text-sm">({clubName(user.club)})</span>
+            </div>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>동아리</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                {Object.keys(Club).map((club) => (
+                  <DropdownMenuItem
+                    key={club}
+                    className="justify-between"
+                    onClick={() => changeClub(club as Club)}
+                  >
+                    {clubName(club)}
+                    {club === user.club && <Check />}
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className="flex text-red-500" onClick={() => Api.Request.removeToken()}>
+            <LogOut color={colors.red['500']} />
+            로그아웃
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
